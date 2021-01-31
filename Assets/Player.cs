8bf7ecac86fd40edf2e8be0127b1bf2d3d3c4723
Mustafa <mustafa@ace.games>
2021-01-31 18:02:09 +0300

    Initial push"

---
+++ Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed =5.0f;
+    public float playerRunSpeed =7.0f;
+
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        float speed = playerSpeed;
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = playerRunSpeed;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),  Input.GetAxis("Vertical"),0);
+        controller.Move(move * Time.deltaTime * speed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward =new Vector3( move.x,move.y,0)+ new Vector3(0.02f,0,0);
+        }
+
+ 
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+         controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
---
+++ Assets/camerafollower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class camerafollower : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(0,0, Random.Range(-8, -3));
+    }
+    
+    void LateUpdate ()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
+        
+        // transform.LookAt(target);
+    }
+
+}
